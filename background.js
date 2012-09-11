d2a1465ebd130168c1650818de13e5a95dfb173a
Anish Shankar <rndanish@gmail.com>
2012-09-11 23:53:15 +0530

    added focus functionality

--- background.js
+++ background.js
@@ -1,55 +1,71 @@
 // Called when the user clicks on the browser action.
 /*
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript(null, {code:"document.body.style.background='red !important'"});
-    chrome.tabs.captureVisibleTab(undefined,function(dataUrl){
-        alert("You just screwed the Background :D\n"+dataUrl);
-    });
-});
-*/
+ chrome.browserAction.onClicked.addListener(function(tab) {
+ chrome.tabs.executeScript(null, {code:"document.body.style.background='red !important'"});
+ chrome.tabs.captureVisibleTab(undefined,function(dataUrl){
+ alert("You just screwed the Background :D\n"+dataUrl);
+ });
+ });
+ */
 
-chrome.browserAction.setBadgeBackgroundColor({color:[0, 200, 0, 100]});
+chrome.browserAction.setBadgeBackgroundColor({
+	color : [0, 200, 0, 100]
+});
 
 var i = 0;
 
 /*
-   chrome.extension.onRequest.addListener(function(req,sender,resp){
-   globstr=String(req.data);
-   });
-   */
-var curval=0;
-cnt=0;
+ chrome.extension.onRequest.addListener(function(req,sender,resp){
+ globstr=String(req.data);
+ });
+ */
+var curval = 0;
+cnt = 0;
 window.setInterval(function() {
-    cnt++;
+	cnt++;
 
-    cur="";
-    chrome.tabs.query({"active":true,"windowId":chrome.windows.WINDOW_ID_CURRENT},function(t){
-        if(typeof t !="undefined")
-    {
-        var hdb={};
-        if(cnt%10==0){
-            hdb=JSON.parse(localStorage.getItem("histdb"));
-            if(hdb==null)hdb={};
-        }
-        for(i=0;i<t.length;i++){
-            cur+=String(t[i].url)+"\n<br/>";
-            if(cnt%10==0){
-                tmp=0;
-                var domain=t[i].url.match(/:\/\/(.[^/]+)/)[1];
+	cur = "";
+	chrome.windows.getCurrent(function(w) {
+		if (w.focused()) {
+			chrome.tabs.query({
+				"active" : true,
+				"windowId" : chrome.windows.WINDOW_ID_CURRENT
+			}, function(t) {
+				if ( typeof t != "undefined") {
+					var hdb = {};
+					if (cnt % 10 == 0) {
+						hdb = JSON.parse(localStorage.getItem("histdb"));
+						if (hdb == null)
+							hdb = {};
+					}
+					for ( i = 0; i < t.length; i++) {
+						cur += String(t[i].url) + "\n<br/>";
+						if (cnt % 10 == 0) {
+							tmp = 0;
+							var domain = t[i].url.match(/:\/\/(.[^/]+)/)[1];
 
-                try{tmp=hdb[domain];}catch(e){}
-                tmp++;
-                hdb[domain]=tmp;
-                curval=tmp;
-            }
-        }
-        if(cnt%10==0){
-            localStorage.setItem("histdb",JSON.stringify(hdb));
-        }
-        localStorage.setItem("hist",cur);
-        i=t.length;
-    }
-    });
-    chrome.browserAction.setBadgeText({text:String(curval)});
+							try {
+								tmp = hdb[domain];
+							} catch(e) {
+							}
+							tmp++;
+							hdb[domain] = tmp;
+							curval = tmp;
+						}
+					}
+					if (cnt % 10 == 0) {
+						localStorage.setItem("histdb", JSON.stringify(hdb));
+					}
+					localStorage.setItem("hist", cur);
+					i = t.length;
+				}
+			});
+
+		}
+	});
+
+	chrome.browserAction.setBadgeText({
+		text : String(curval)
+	});
 
 }, 100);
